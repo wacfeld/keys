e046750021ffb6656a5f2c3862354f318b77163e
Daniel Yu <wacfeldwang333@gmail.com>
2024-09-21 18:23:15 -0400

    595 daisy chain testing

--- pi/595.py
+++ pi/595.py
@@ -1,5 +1,42 @@
 import pigpio
 import time
+
+DATA = 10
+CLOCK = 11
+LATCH = 8
+
 pi = pigpio.pi()
 
-pi.set_mode(4, 1)
+# bit bang a byte
+def shift(A):
+    pi.write(CLOCK, 0)
+    pi.write(LATCH, 0)
+    
+    for i in range(7, -1, -1):
+        pi.write(CLOCK, 0)
+        pi.write(DATA, int(A[i]))
+        pi.write(CLOCK, 1)
+    
+    pi.write(CLOCK, 0)
+    pi.write(LATCH, 1)
+
+# bit bang a moving light pattern
+def knightrider():
+    A = [1,0,0,1,0,1,1,0,0,1,1,0,1,0,0,1]
+    # reset
+    pi.write(CLOCK, 0)
+    pi.write(LATCH, 0)
+    
+    i = 0
+    while True:
+        i = i % 16
+        val = 0 if i < 8 else 1
+        
+        pi.write(DATA, val)
+        pi.write(CLOCK, 1)
+        pi.write(CLOCK, 0)
+        pi.write(LATCH, 1)
+        pi.write(LATCH, 0)
+        time.sleep(0.05)
+        i += 1
+knightrider()
