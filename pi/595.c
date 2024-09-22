2570038dbb93854f402b7c48186a2552e63e9005
Daniel Yu <wacfeldwang333@gmail.com>
2024-09-22 02:17:28 -0400

    C gettime() wrapper

--- pi/595.c
+++ pi/595.c
@@ -1,6 +1,17 @@
 #include <stdio.h>
 #include <pigpio.h>
+#include <unistd.h>
+
+typedef unsigned long ulong;
+
+// return the current unix time in nanoseconds
+unsigned long gettime() {
+    struct timespec time;
+    clock_gettime(CLOCK_REALTIME, &time);
+    return time.tv_sec*1000000000 + time.tv_nsec;
+}
 
 int main() {
-    printf("hello world!");
+    //gpioInitialise();
+    //gpioTerminate();
 }
