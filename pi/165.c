52c09324f27fa0e4f912f39b3456164bbae05d79
Daniel Yu <wacfeldwang333@gmail.com>
2024-09-22 23:48:01 -0400

    finish 165 time_spi()

--- pi/165.c
+++ pi/165.c
@@ -53,12 +53,23 @@ void printbytes(unsigned char *bytes, int n) {
     putchar('\n');
 }
 
-void time_spi(int baud) {
+void time_spi(int baud, int n) {
     int h = spiOpen(0, baud, 4); // binary 100 sets CE0 to active high
 
-    char buf[1];
-    spiRead(h, buf, 1);
-    printbytes((unsigned char*)buf, 1);
+    ulong total = 0;
+
+    ulong a = gettime();
+    for(int i = 0; i < n; i++) {
+        char buf[2];
+        spiRead(h, buf, 2);
+        total += buf[0] + buf[1];
+        //printbytes((unsigned char*)buf, 2);
+    }
+    ulong b = gettime();
+
+    ulong ms = (b-a)/1000000;
+    printf("SPI at %d baud took %ld ms to make %d reads, i.e. %ld reads a second\n", baud, ms, n, 1000*n/ms);
+    printf("the total is %ld\n", total);
 
     spiClose(h);
 }
@@ -69,10 +80,7 @@ int main() {
     //int buf[8];
     //bang(8, buf);
     //printarr(buf, 8);
-    while(1) {
-        time_spi(100000);
-        usleep(100000);
-    }
+    time_spi(5000000, 10000);
 
     gpioTerminate();
 }
