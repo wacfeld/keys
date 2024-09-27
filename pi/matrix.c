270aef8113285d01e96030433ca53ca12347ad45
Daniel Yu <wacfeldwang333@gmail.com>
2024-09-27 01:55:38 -0400

    simple matrix.c

--- pi/matrix.c
+++ pi/matrix.c
@@ -7,15 +7,35 @@
 // - the 165 is CE1
 
 // 24 keys requires a 5x5 keyboard matrix
-#define NROWS 5
-#define NKEYS 24
+#define N_OUT 5
+#define N_IN 5
+#define N_KEYS 24
 
 // SPI handles
 int OUT, IN;
 
 // poll all the keys and write to the output buffer
-void poll(int *buf) {
-    
+void poll(char *buf) {
+    int c = 0;
+    for(int i = 0; i < N_OUT; i++) {
+        char data = 1 << i;
+        spiWrite(OUT, &data, 1);
+
+        spiRead(IN, &data, 1);
+        for(int j = 0; j < N_IN && c < N_KEYS; j++) {
+            // transpose the result (inputs vary slower than outputs)
+            buf[j*N_OUT+i] = data % 2;
+            data /= 2;
+            c++;
+        }
+    }
+}
+
+void printbuf(char *buf) {
+    for(int i = 0; i < N_KEYS; i++) {
+        printf("%d", buf[i]);
+    }
+    putchar('\n');
 }
 
 int main() {
@@ -26,7 +46,12 @@ int main() {
     OUT = spiOpen(0, 1000000, 8);
     IN  = spiOpen(1, 1000000, 8);
 
-    
+    char buf[N_KEYS];
+    while(1) {
+        poll(buf);
+        printbuf(buf);
+        usleep(1000);
+    }
 
     // cleanup
     spiClose(OUT);
