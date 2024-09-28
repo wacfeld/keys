2f56a20598398ac546c88582900998de04a52aeb
Daniel Yu <wacfeldwang333@gmail.com>
2024-09-28 00:19:00 -0400

    work on matrix/map stuff

--- aux/main.cc
+++ aux/main.cc
@@ -2,10 +2,13 @@
 
 #include <SineWave.h>
 #include <RtAudio.h>
+#include <map>
 
 #include "matrix.h"
 
-#define NKEYS 25
+#define N_KEYS 25
+#define N_OUT 5
+#define N_IN 5
 
 // e to e containing middle c
 double freqs[NKEYS] = {
@@ -27,19 +30,8 @@ double freqs[NKEYS] = {
 using namespace stk;
 typedef unsigned int uint;
 
-// make an alias for the callback function prototype used by stk
+// make an alias for the callback function prototype used by stk (tick() is an example)
 typedef int (*callback)(void*, void*, uint, double, RtAudioStreamStatus, void*);
-
-
-int tick(void *output, void *input, uint nframes, double streamTime, RtAudioStreamStatus status, void *data) {
-    SineWave *sine = (SineWave *) data;
-    StkFloat *out = (StkFloat *) output;
-    for(unsigned long int i = 0; i < nframes; i++) {
-        *out = sine->tick();
-        out++;
-    }
-    return 0;
-}
 
 void init(RtAudio *out, callback f, void *data) {
     Stk::setSampleRate(44100.0);
@@ -61,17 +53,55 @@ void cleanup(RtAudio *out) {
     }
 }
 
+int tick(void *output, void *input, uint nframes, double streamTime, RtAudioStreamStatus status, void *data) {
+    SineWave *sine = (SineWave *) data;
+    StkFloat *out = (StkFloat *) output;
+    for(unsigned long int i = 0; i < nframes; i++) {
+        *out = sine->tick();
+        out++;
+    }
+    return 0;
+}
+
+// poll the matrix and update the data accordingly
+void update(std::map<int, SineWave> &&data, Matrix *mat) {
+
+    // check the status of every key against the hash map
+    poll(mat);
+    for(int i = 0; i < mat->keys; i++) {
+        // key was just pressed
+        if(mat->buf[i] == 1 && data.count(i) == 0) {
+            data[i] = SineWave();
+            data[i].setFrequency(freqs[i]);
+        }
+
+        // key was just released
+        else if(mat->buf[i] == 0 && data.count(i) > 0) {
+            data.erase(i);
+        }
+    }
+}
+
 int main()
 {
-    //RtAudio out;
-    //init(&out, tick, data);
+    RtAudio out;
+
+    std::map<int, SineWave> data;
+    
+    char buf[N_KEYS];
+    Matrix *mat = {.out=N_OUT, .in=N_IN, .keys=N_KEYS, .buf=buf};
+
+    initMatrix();
+    init(&out, tick, (void*) &data);
 
     //if(out.startStream()) {
     //    std::cout << out.getErrorText() << std::endl;
     //    exit(1);
     //}
 
-    //cleanup(&out);
+    cleanup(&out);
+
+    cleanupMatrix();
 
     return 0;
 }
