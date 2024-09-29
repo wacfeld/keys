3fa4b9c432c79649fe7776524636cad279f801e3
Daniel Yu <wacfeldwang333@gmail.com>
2024-09-29 02:17:36 -0400

    wavetable start

---
+++ aux/wavetable.h
@@ -0,0 +1,16 @@
+#ifndef WAVETABLE_H
+#define WAVETABLE_H
+#include <Generator.h>
+
+class WaveTable : public stk::Generator
+{
+public:
+    WaveTable(void);
+    ~WaveTable(void);
+
+    void reset(void);
+
+    //void setRate(StkFloat rate) { 
+};
+
+#endif
