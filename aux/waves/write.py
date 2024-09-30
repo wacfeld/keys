28050ec1da4cc85b044c023a0f7c53040a8cef37
Daniel Yu <wacfeldwang333@gmail.com>
2024-09-30 01:47:24 -0400

    fix typos

--- aux/waves/write.py
+++ aux/waves/write.py
@@ -2,9 +2,12 @@ import struct
 
 # write binary data to file
 # data is a list of numbers
-def write(data, fname):
+def write(fname, data):
+    if not fname.endswith('.raw'):
+        print(f'filename {fname} does not end in ".raw"')
+        exit(1)
     # pack into big-endian 16-bit data
-    content = struct.pack('>' + 'h' * len(data))
+    content = struct.pack('>' + 'h' * len(data), *data)
 
     # write
     print(f'writing {len(data)} samples to {fname}')
