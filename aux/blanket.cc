0d3d8f9910f206295806d21adb7e2bf81a835787
Daniel Yu <wacfeldwang333@gmail.com>
2024-10-01 19:07:30 -0400

    Blanket work

--- aux/blanket.cc
+++ aux/blanket.cc
@@ -1,1 +1,44 @@
 #include "blanket.h"
+
+// default constructor sets the most simple envelope possible
+Blanket::Blanket(void) {
+    state = IDLE;
+    index = 0;
+
+    // instant attack with a target/sustain of 1
+    opening.emplace_back(-1, 1);
+    // instant release
+    closing.emplace_back(-1, 0);
+
+    Stk::addSampleRateAlert(this);
+}
+
+Blanket::Blanket(std::string shape) {
+    state = IDLE;
+    index = 0;
+
+    setShape(shape);
+
+    Stk::addSampleRateAlert(this);
+}
+
+Blanket::~Blanket(void) {
+    Stk::removeSampleRateAlert(this);
+}
+
+Blanket::setShape(std::string shape) {
+
+}
+
+stk::StkFloat Blanket::tick(void) {
+    return 0;
+}
+
+stk::StkFrames &Blanket::tick(stk::StkFrames &frames, unsigned int channel) {
+
+    return frames;
+}
+
+void Blanket::sampleRateChanged(stk::StkFloat newRate, stk::StkFloat oldRate) {
+    
+}
--- aux/blanket.h
+++ aux/blanket.h
@@ -6,11 +6,13 @@
 
 #include <Generator.h>
 #include <string>
+#include <vector>
 
-namespace stk {
-
-class Blanket: public Generator {
+class Blanket: public stk::Generator {
 public:
+    enum State {OPENING, CLOSING, SUSTAIN, IDLE};
+    enum State state;
+
     Blanket(void);
     Blanket(std::string shape);
     ~Blanket(void);
@@ -20,13 +22,19 @@ public:
     
     void setShape(std::string shape);
 
-    StkFloat lastOut(void) const {return lastFrame_[0];}
+    stk::StkFloat lastOut(void) const {return lastFrame_[0];}
 
-    StkFloat tick(void);
-    StkFrames &tick(StkFrames &frames, unsigned int channel=0);
+    stk::StkFloat tick(void);
+    stk::StkFrames &tick(stk::StkFrames &frames, unsigned int channel=0);
 protected:
+    void sampleRateChanged(stk::StkFloat newRate, stk::StkFloat oldRate);
+
+    // vectors of (rate, target) pairs for the when the note is held (opening) and released (closing)
+    // a rate of -1 stands for infinity (instantaneous change)
+    std::vector<std::pair<stk::StkFloat, stk::StkFloat>> opening;
+    std::vector<std::pair<stk::StkFloat, stk::StkFloat>> closing;
+
+    // when the state is OPENING or CLOSING, the index tells us what part of the opening/closing vectors to look at
+    int index;
 };
-
-}
-
 #endif
