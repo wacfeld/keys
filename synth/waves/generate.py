431e64593e43bd048f5b6b4887064a1f102295a4
Daniel Yu <wacfeldwang333@gmail.com>
2024-10-09 04:38:20 -0400

    quads

--- synth/waves/generate.py
+++ synth/waves/generate.py
@@ -8,7 +8,12 @@ if __name__ == '__main__':
     write(triangle(), 'triangle.raw', overwrite=True)
     write(sawtooth(), 'sawtooth.raw', overwrite=True)
 
-    # marimbas
-    write(marimba(), 'marimba.raw', overwrite=True)
-    write(marimba2(), 'marimba2.raw', overwrite=True)
-    write(marimba3(), 'marimba3.raw', overwrite=True)
+    write(pulse(10), "pulse10.raw", overwrite=True)
+    write(pulse(20), "pulse20.raw", overwrite=True)
+    write(pulse(30), "pulse30.raw", overwrite=True)
+    write(pulse(40), "pulse40.raw", overwrite=True)
+
+    write(quad(triangle), 'quadtriangle.raw', overwrite=True)
+    write(quad(sine), 'quadsine.raw', overwrite=True)
+    write(quad(square), 'quadsquare.raw', overwrite=True)
+    write(quad(sawtooth), 'quadsawtooth.raw', overwrite=True)
