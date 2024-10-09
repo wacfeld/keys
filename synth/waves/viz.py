5ac471898758a34854f02f4af913645bcff0f6af
Daniel Yu <wacfeldwang333@gmail.com>
2024-10-09 06:16:15 -0400

    clean up freqs() and make axes logarithmic

--- synth/waves/viz.py
+++ synth/waves/viz.py
@@ -31,32 +31,19 @@ def freqs(data, title=None, threshold=0.001):
     ft = np.abs(fft(data))
     pos = ft[:len(ft)//2]
 
-    ## remove trailing tiny values
-    #if threshold != None:
-    #    # get max
-    #    m = np.max(pos)
+    plt.scatter(np.arange(len(pos)), pos)
+    ax = plt.gca()
+    ax.set_xscale('log')
+    ax.set_yscale('log')
 
-    #    # array of booleans flagging very small values
-    #    small = (pos/m < threshold)
-    #    # indices of non-small values
-    #    nonsmalls = np.where(np.logical_not(small))[0]
-    #    if len(nonsmalls) != 0:
-    #        # get index of last non-small value, add 2, and cap at length of array
-    #        i = min(nonsmalls[-1] + 2, len(pos))
-    #        pos = pos[:i]
-
-    # make y axis logarithmic
-    pos = 10*np.log10(pos)
-    pos[pos==-inf] = -200
-
-    plt.plot(pos)
     if title is not None:
         plt.title(title)
-    plt.xlabel('Hz')
-    plt.ylabel('db')
-    plt.ylim(bottom=-100)
-    #plt.xlim(right=50)
-    plt.xlim(left=0)
+    plt.xlabel('freq')
+    plt.ylabel('amplitude')
+
+    plt.axvline(x=1)
+    plt.axhline(y=1)
+
     plt.show()
 
 if __name__ == '__main__':
