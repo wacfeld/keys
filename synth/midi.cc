0e020c4ee52526e6ce106e10cc0511eeae2111f1
Daniel Yu <wacfeldwang333@gmail.com>
2024-10-15 03:56:13 -0400

    MidiOut class

--- synth/midi.cc
+++ synth/midi.cc
@@ -1,1 +1,11 @@
-#include <RtMidi.h>
+#include "midi.h"
+
+MidiOut::MidiOut() {
+    try {
+        out = new RtMidiOut();
+    } catch(RtMidiError &err) {
+        puts("MidiOut encountered the following error when trying to initialize:");
+        err.printMessage();
+        exit(1);
+    }
+}
--- synth/midi.h
+++ synth/midi.h
@@ -1,4 +1,17 @@
 #ifndef MIDI_H
 #define MIDI_H
+#include <RtMidi.h>
+
+class MidiOut {
+public:
+    MidiOut(void);
+    ~MidiOut(void);
+
+    void noteOn();
+    void noteOff();
+
+private:
+    RtMidiOut *out;
+};
 
 #endif
