7bed3cbb5ae2762e4654bcb55d5e3bf0275c89bf
Daniel Yu <wacfeldwang333@gmail.com>
2024-10-16 18:15:08 -0400

    abbreviate assert inequalities

--- mini/midi.py
+++ mini/midi.py
@@ -5,30 +5,30 @@ def write(*args):
     _out.write(bytes(args))
 
 def noteOn(note, vel=127, channel=0):
-    assert 0 <= note and note <= 127
-    assert 0 <= vel and vel <= 127
-    assert 0 <= channel and channel < 16
+    assert 0 <= note <= 127
+    assert 0 <= vel <= 127
+    assert 0 <= channel < 16
     write(0x90 + channel, note, vel)
 
 def noteOff(note, vel=127, channel=0):
-    assert 0 <= note and note <= 127
-    assert 0 <= vel and vel <= 127
-    assert 0 <= channel and channel < 16
+    assert 0 <= note <= 127
+    assert 0 <= vel <= 127
+    assert 0 <= channel < 16
     write(0x80 + channel, note, vel)
 
 def setInst(inst, channel=0):
-    assert 0 <= inst and inst <= 127
-    assert 0 <= channel and channel < 16
+    assert 0 <= inst <= 127
+    assert 0 <= channel < 16
     write(0xC0 + channel, inst)
 
 def setVol(vol, channel=0):
-    assert 0 <= vol and vol <= 127
-    assert 0 <= channel and channel < 16
+    assert 0 <= vol <= 127
+    assert 0 <= channel < 16
     write(0xB0 + channel, 7, vol)
 
 def pitchBend(val, channel=0):
-    assert 0 <= val and val < 0x4000
-    assert 0 <= channel and channel < 16
+    assert 0 <= val < 0x4000
+    assert 0 <= channel < 16
     L = val & 0x7F
     M = val >> 7
     message(0xE0 + channel, L, M)
