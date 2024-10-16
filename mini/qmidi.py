bc7fd2cd94916c483098302a7c31f89f126f9217
Daniel Yu <wacfeldwang333@gmail.com>
2024-10-16 18:35:22 -0400

    fill out qmidi.py

--- mini/qmidi.py
+++ mini/qmidi.py
@@ -1,13 +1,17 @@
 import midi
 
 def noteOn(note, vel=127):
-    pass
+    assert 0 <= note <= 255
+    midi.noteOn(note // 2, vel, note % 2)
 
 def noteOff(note, vel=127):
-    pass
+    assert 0 <= note <= 255
+    midi.noteOff(note // 2, vel, note % 2)
 
 def setVol(vol):
-    pass
+    midi.setVol(vol, 0)
+    midi.setVol(vol, 1)
 
 def setInst(inst):
-    pass
+    midi.setInst(inst, 0)
+    midi.setInst(inst, 1)
